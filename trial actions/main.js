c52a99fc5fc58eac016c68c759aac2bf20241c4d
DavydMarkosian <markdaiv97@gmail.com>
2022-11-05 19:30:31 +0200

    trial

---
+++ trial actions/main.js
@@ -0,0 +1,8 @@
+let firstObject = {a: 5, b: 'sometext'};
+console.log(firstObject);
+let copyOfObject = firstObject;
+console.log(copyOfObject);
+copyOfObject.a=10;
+console.log(copyOfObject);
+copyOfObject.c=2>1;
+console.log(copyOfObject);
