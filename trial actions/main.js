09e94b182ffd3ae2d21a0a1894b69740307399b3
DavydMarkosian <markdaiv97@gmail.com>
2022-11-05 21:27:15 +0200

    trial

--- trial actions/main.js
+++ trial actions/main.js
@@ -2,7 +2,20 @@ let firstObject = {a: 5, b: 'sometext'};
 console.log(firstObject);
 let copyOfObject = firstObject;
 console.log(copyOfObject);
-copyOfObject.a=10;
+copyOfObject.a = 10;
 console.log(copyOfObject);
-copyOfObject.c=2>1;
+copyOfObject.c = 2 > 1;
 console.log(copyOfObject);
+
+const data = {city: 'Madrid'};
+console.log(data);
+data.city = 'Barcelona';
+console.log(data);
+data.direction = 'Antonio Leyva';
+data.time = 'for 1 years';
+console.log(data);
+delete data.city;
+console.log(data)
+data['city'] = 'Anycity';
+console.log(data);
+
