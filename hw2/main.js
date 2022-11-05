e32ea81b0fcc26f3562357ffbc8df9ce63185e9b
DavydMarkosian <markdaiv97@gmail.com>
2022-11-05 22:25:13 +0200

    js_hw2

---
+++ hw2/main.js
@@ -0,0 +1,69 @@
+//Exercise 1
+let firstArray = [1, 2, 3, 4, 5, 6, 7, true, 'word', false]
+console.log('Exercise 1:');
+console.log(firstArray);
+
+//Exercise 2-3
+let title1 = 'title1';
+let pageCount1 = 'pageCount1';
+let genre1 = 'genre1';
+let author1 = [name = 'FIO1', age = 'AGE1'];
+let book1 = {
+    title1,
+    pageCount1,
+    genre1,
+    author1
+};
+let title2 = 'title2';
+let pageCount2 = 'pageCount2';
+let genre2 = 'genre2';
+let author2 = [name = 'FIO2', age = 'AGE2'];
+let book2 = {
+    title2,
+    pageCount2,
+    genre2,
+    author2
+
+};
+let title3 = 'title3';
+let pageCount3 = 'pageCount3';
+let genre3 = 'genre3';
+let author3 = [name = 'FIO3', age = 'AGE3'];
+let book3 = {
+    title3,
+    pageCount3,
+    genre3,
+    author3
+};
+console.log('Exercise 2-3:');
+console.log(book1, book2, book3);
+
+//exercise 4
+let user1 = {name: 'name1', username: 'username1', password: 'user`s1 password'}
+let user2 = {name: 'name2', username: 'username2', password: 'user`s2 password'}
+let user3 = {name: 'name3', username: 'username3', password: 'user`s3 password'}
+let user4 = {name: 'name4', username: 'username4', password: 'user`s4 password'}
+let user5 = {name: 'name5', username: 'username5', password: 'user`s5 password'}
+let user6 = {name: 'name6', username: 'username6', password: 'user`s6 password'}
+let user7 = {name: 'name7', username: 'username7', password: 'user`s7 password'}
+let user8 = {name: 'name8', username: 'username8', password: 'user`s8 password'}
+let user9 = {name: 'name9', username: 'username9', password: 'user`s9 password'}
+let user10 = {name: 'name10', username: 'username10', password: 'user`s10 password'}
+
+let arrUsers =
+    [user1, user2, user3, user4, user5, user6, user7, user8, user9, user10];
+console.log('exercise 4:')
+console.log(user1.password)
+console.log(user2.password)
+console.log(user3.password)
+console.log(user3.password)
+console.log(user4.password)
+console.log(user5.password)
+console.log(user6.password)
+console.log(user7.password)
+console.log(user8.password)
+console.log(user9.password)
+console.log(user10.password)
+
+
+
